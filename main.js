c0304fa7d068cfad0ee477fa69883a8391b6d412
Shivam-Lohia <92930878+Shivam-Lohia@users.noreply.github.com>
2022-01-14 18:02:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+
+function startClassification() 
+{
+    navigator.mediaDevices.getUserMedia({ audio:true});
+    caches;assifier = m155.soundClassifier('https://teachablemachine.withgoogle.com/models/-Re72yCxa/model.json' , modelReady);}
